c8e84e412c5d74719f43e76374aa8086e1e7f24e
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-04-09 14:16:12 -0400

    update collections

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import Api from '../modules/Api';
+import shortid from 'shortid';
 
 class Collections extends Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      isReady: false
+      isReady: false,
+      records: [],
+      selectedRecord: null
     };
+    this.onRecordClick = this.onRecordClick.bind(this);
   }
   
+  onRecordClick(e) {
+    let record = this.state.records[e.target.getAttribute('index')];
+    this.setState({record});
+  }
+
   componentDidMount() {
-    Api.getFullURL('http://api.freegenes.org/collections/')
+    Api.get('/collections/')
     .then(res => {
-      console.log('/collections', res)
+      console.log('/collections', res);
+      this.setState({records: res});
     })
     .catch(error => {
       throw error;
@@ -22,6 +32,19 @@ class Collections extends Component {
   }
 
   render() {
+    const records = this.state.records;
+    const recordListItems = records.map((record, recordIndex) => {
+      return (
+        <button 
+          key={shortid.generate()}
+          className="list-group-item list-group-item-action"
+          index={recordIndex}
+          onClick={this.onRecordClick}
+        >
+          {record.name}
+        </button>
+      );
+    });
     return (
       <div className="Collections container-fluid">
         <div className="row">
@@ -34,14 +57,24 @@ class Collections extends Component {
                 Collections View.
               </div>
               <ul className="list-group list-group-flush">
-                <Link 
-                  to="/"
-                  className="list-group-item list-group-item-action"
-                >
-                  Home
-                </Link>
+                {recordListItems}
               </ul>
             </div>
+          </div>
+          <div className="col-xs-12 col-md-6">
+            {this.state.record && (
+              <div className="card mt-3">
+                <div className="card-header">
+                  {this.state.record.name}
+                </div>
+                <div className="card-body">
+                  <pre>{JSON.stringify(this.state.record, null, 2)}</pre>
+                </div>
+                {/* <ul className="list-group list-group-flush">
+                  {recordListItems}
+                </ul> */}
+              </div>
+            )}
           </div>
         </div>
       </div>
