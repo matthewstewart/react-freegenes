00b255eee5caead83516ab7839c1159e99f6e568
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-04-10 18:53:18 -0400

    change part toggle

--- src/components/PartListItem.jsx
+++ src/components/PartListItem.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Markdown from 'react-markdown';
 import shortid from 'shortid';
 import Sequence from './Sequence';
@@ -20,25 +20,34 @@ class PartListItem extends Component {
     const partIsText = typeof part === 'string';
     const partID = partIsText ? part : part.uuid;
     const route = `/collections/${collectionID}/parts/${partID}`;
+    const prevRoute = `/collections/${collectionID}`;
     const paramCollectionID = this.props.match.params.recordId;
     const paramPartID = this.props.match.params.partId;
     const isSelected = collectionID === paramCollectionID && partID === paramPartID;
     if (isSelected) {
       let tags = null;
       return (
-        <div className="card-body">
-          <h4 className="card-title">{part.name}</h4>
-          {part.time_created && <><strong>Created</strong>: {part.time_created}<br/></>}
-          {part.time_updated && <><strong>Updated</strong>: {part.time_updated}<br/></>}
-          {part.gene_id && <><strong>Gene ID</strong>: {part.gene_id}<br/></>}
-          {part.description && <Markdown source={part.description} />}
-          {part.full_sequence && <Sequence title="Full Sequence" content={part.full_sequence}/>}
-          {part.optimized_sequence && <Sequence title="Optimized Sequence" content={part.optimized_sequence}/>}
-          {part.original_sequence && <Sequence title="Original Sequence" content={part.original_sequence}/>}
-          {part.synthesized_sequence && <Sequence title="Synthesized Sequence" content={part.synthesized_sequence}/>}
-          {part.tags && part.tags.length > 0 && <>{tags}</>}
-          {/* <pre>{JSON.stringify(part, null, 2)}</pre> */}
-        </div>
+        <>
+          <Link 
+            className="list-group-item list-group-item-action"
+            to={prevRoute}
+          >
+            {part.name}
+          </Link>        
+          <div className="card-body">
+            <h4 className="card-title">{part.name}</h4>
+            {part.time_created && <><strong>Created</strong>: {part.time_created}<br/></>}
+            {part.time_updated && <><strong>Updated</strong>: {part.time_updated}<br/></>}
+            {part.gene_id && <><strong>Gene ID</strong>: {part.gene_id}<br/></>}
+            {part.description && <Markdown source={part.description} />}
+            {part.full_sequence && <Sequence title="Full Sequence" content={part.full_sequence}/>}
+            {part.optimized_sequence && <Sequence title="Optimized Sequence" content={part.optimized_sequence}/>}
+            {part.original_sequence && <Sequence title="Original Sequence" content={part.original_sequence}/>}
+            {part.synthesized_sequence && <Sequence title="Synthesized Sequence" content={part.synthesized_sequence}/>}
+            {part.tags && part.tags.length > 0 && <>{tags}</>}
+            {/* <pre>{JSON.stringify(part, null, 2)}</pre> */}
+          </div>
+        </>
       );
     } else {
       return (
@@ -47,11 +56,7 @@ class PartListItem extends Component {
           className="list-group-item list-group-item-action"
           to={route}
         >
-          {isSelected ? (
-            <>Selected - {part.name}</>
-          ) : (
-            <>{part.name}</>
-          )}
+          {part.name}
         </NavLink>
       );
     }  
