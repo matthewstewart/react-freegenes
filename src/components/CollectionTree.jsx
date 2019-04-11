9f6544a16bfbfc68a99b8007b571a2944604fed3
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-04-11 11:40:33 -0400

    move tree data gen to helper functions

--- src/components/CollectionTree.jsx
+++ src/components/CollectionTree.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import shortid from 'shortid';
 import Api from '../modules/Api';
+import Tree from '../modules/Tree';
 import { Treebeard } from 'react-treebeard';
 import CollectionTreeStyles from './CollectionTreeStyles';
 
@@ -54,109 +55,7 @@ class CollectionTree extends Component {
 
   treebeardize(collection) {
     console.log('treebeardize', collection);
-    let result = {
-      name: `Collection: ${collection.name}`,
-      toggled: true,
-      children: [
-        {
-          name: 'Sub-Collections',
-          children: []
-        }
-        // {
-        //   name: 'loading parent',
-        //   loading: true,
-        //   children: [] 
-        // } 
-      ]
-    };
-    const subcollections = collection.subcollections || [];
-    for (let i = 0; i < subcollections.length; i++) {
-      const subcollection = subcollections[i];
-      const subCollectionObj = {
-        name: subcollection.name,
-        children: [
-          {
-            name: 'Parts',
-            children: []
-          }
-        ]
-      };
-      const parts = subcollection.parts;
-      for (let j = 0; j < parts.length; j++) {
-        const part = parts[j];
-        const partObj = {
-          name: part.name,
-          children: []
-        };
-        part.time_created && partObj.children.push({ name: `Created: ${part.time_created}`});
-        part.time_updated && partObj.children.push({ name: `Updated: ${part.time_updated}`});
-        part.description && partObj.children.push({ name: `${part.description}`});
-        part.status && partObj.children.push({ name: `Status: ${part.status}`});
-        part.gene_id && partObj.children.push({ name: `Gene ID: ${part.gene_id}`});
-        part.part_type && partObj.children.push({ name: `Part Type: ${part.part_type}`});
-        part.primer_for && partObj.children.push({ name: `Primer For: ${part.primer_for}`});
-        part.primer_rev && partObj.children.push({ name: `Primer Rev: ${part.primer_rev}`});
-        part.original_sequence && partObj.children.push({ 
-          name: `Original Sequence`,
-          children: [
-            { name: `${part.original_sequence}`}
-          ]
-        });
-        part.optimized_sequence && partObj.children.push({ 
-          name: `Optimized Sequence`,
-          children: [
-            { name: `${part.optimized_sequence}`}
-          ]
-        });
-        part.synthesized_sequence && partObj.children.push({ 
-          name: `Synthesized Sequence`,
-          children: [
-            { name: `${part.synthesized_sequence}`}
-          ]
-        });
-        part.full_sequence && partObj.children.push({ 
-          name: `Full Sequence`,
-          children: [
-            { name: `${part.full_sequence}`}
-          ]
-        });
-        part.genbank && partObj.children.push({ 
-          name: `Genbank`,
-          children: [
-            { name: `EC_number: ${part.genbank.EC_number}`},
-            { name: `GenBank_acc: ${part.genbank.GenBank_acc}`},
-            { name: `Source: ${part.genbank.Source}`},
-            { name: `gene: ${part.genbank.gene}`},
-            { name: `gene_synonyms: ${part.genbank.gene_synonyms}`},
-            { name: `locus_tag: ${part.genbank.locus_tag}`},
-            { name: `note: ${part.genbank.note}`},
-            { name: `product: ${part.genbank.product}`},
-            { name: `protein_id: ${part.genbank.protein_id}`},
-            { name: `translation: ${part.genbank.translation}`}
-          ]
-        });
-        part.barcode && partObj.children.push({ name: `Barcode: ${part.barcode}`});
-        part.translation && partObj.children.push({ name: `Translation: ${part.translation}`});
-        part.vbd && partObj.children.push({ name: `VBD: ${part.vbd}`});
-        part.vector && partObj.children.push({ name: `Vector: ${part.vector}`});
-        part.tags && partObj.children.push({ name: `Tags: ${part.tags}`});
-        subCollectionObj.children[0].children.push(partObj);
-      }
-      subCollectionObj.children[0].children.sort((a, b) => {
-        if (a.name > b.name) { return 1; }
-        if (a.name < b.name) { return -1; }
-        return 0;
-      });
-      subCollectionObj.children[0].name = `Parts (${subCollectionObj.children[0].children.length})`;
-      result.children[0].children.push(subCollectionObj);
-    }
-    result.children[0].children.sort((a, b) => {
-      if (a.name > b.name) { return 1; }
-      if (a.name < b.name) { return -1; }
-      return 0;
-    });
-    result.children[0].name = `Sub-Collections (${result.children[0].children.length})`;
-    return result;
+    return Tree.convertCollection(collection);
   }
 
   onToggle(node, toggled){
